440cfeb6167a29bb1581c2532fd57d5c753af543
techniker <tec@hellercom.de>
2014-09-02 01:47:54 +0200

    fixes

--- thinkblink.c
+++ thinkblink.c
@@ -1,6 +1,7 @@
-# Copyright (c) 2010, Bjoern Heller <tec@hellercom.de>. All rights reserved
-# This code is licensed under GNU/ GPL
-
+/*
+Copyright (c) 2010, Bjoern Heller <tec@hellercom.de>. All rights reserved
+This code is licensed under GNU/ GPL
+*/
 #include <stdio.h>
 
 const char FILENAME[] = "/proc/acpi/ibm/light";
